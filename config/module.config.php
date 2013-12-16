373dfd942d362088822af92c42f2db9fff46bbfe
Dario <dario@dev.dev>
2013-12-16 15:40:45 -0300

    Fix

--- config/module.config.php
+++ config/module.config.php
@@ -1,29 +1,6 @@
 <?php
 return array(
     'factories' => array(
-        'Desyncr\Connected\Zmq\Service\ZmqService'  => 'Desyncr\Connected\Zmq\Factory\ZmqServiceFactory'
-    ),
-
-    'controllers' => array(
-        'invokables' => array(
-            'Desyncr\Connected\Zmq\Controller\Daemon'    => 'Desyncr\Connected\Zmq\Controller\DaemonController',
-        )
-    ),
-
-    'console' => array(
-        'router' => array(
-            'routes' => array(
-                'zmq_daemon_route' => array(
-                    'options' => array(
-                        'route' => 'zmq daemon execute',
-                        'defaults' => array(
-                            '__NAMESPACE__' => 'Desyncr\Connected\Zmq\Controller',
-                            'controller' => 'Daemon',
-                            'action' => 'execute'
-                        )
-                    )
-                )
-            )
-        )
+        'Desyncr\Connected\Doctrine\Service\DoctrineService'  => 'Desyncr\Connected\Doctrine\Factory\DoctrineServiceFactory'
     ),
 );
--- src/Desyncr/Connected/Doctrine/Entity/Notification.php
+++ src/Desyncr/Connected/Doctrine/Entity/Notification.php
@@ -1,6 +1,5 @@
 <?php
 namespace Desyncr\Connected\Doctrine\Entity;
-use Desyncr\Connected\Frame as Connected;
 
 class Notification {
 }
