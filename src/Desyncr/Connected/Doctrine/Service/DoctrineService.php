a0074704e32ce26b41c9807e640f309ae6bfbf23
Dario <dario@dev.dev>
2013-12-17 16:07:27 -0300

    Fix

--- src/Desyncr/Connected/Doctrine/Service/DoctrineService.php
+++ src/Desyncr/Connected/Doctrine/Service/DoctrineService.php
@@ -1,19 +1,20 @@
 <?php
 namespace Desyncr\Connected\Doctrine\Service;
 use Desyncr\Connected\Service as Connected;
+use Desyncr\Connected\Doctrine\Entity;
 
 class DoctrineService extends Connected\AbstractService {
     public function dispatch() {
 
-        $notification = new \Desyncr\Connected\Doctrine\Entity\Notification();
         foreach ($this->frames as $frame) {
+            $notification = new Notification();
             $notification->setBody($frame->get('title'));
             if ($type = $frame->get('type')) {
                 $notification->setType($type);
             }
+            $this->em->persist($notification);
         }
 
-        $this->em->persist($notification);
         $this->em->flush();
     }
 
